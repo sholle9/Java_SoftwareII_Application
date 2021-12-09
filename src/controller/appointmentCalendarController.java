c73d46ad5e3c8c61a7326ed07d7d95673418f644
sholle9 <sholle9@wgu.edu>
2021-12-09 00:15:14 +0000

    fixed launchin issues by downloading correct lib and creating path variable

---
+++ src/controller/appointmentCalendarController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class appointmentCalendarController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
