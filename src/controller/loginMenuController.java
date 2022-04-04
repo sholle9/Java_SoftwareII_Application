fdf23ebfa5eecdb5d056be9bbce8bacff55ef17a
sholle9 <sholle9@wgu.edu>
2022-04-03 20:41:53 -0400

    Created pop-up dialog boxes for the error messages on the login screen

--- src/controller/loginMenuController.java
+++ src/controller/loginMenuController.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 import helper.DBQuery;
@@ -17,6 +18,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -42,6 +44,42 @@ public class loginMenuController implements Initializable {
     Parent scene;
 
 
+    //Observable list method for getting the data from the database
+    public ObservableList<users> usersList(){
+        ObservableList <users> usersList = FXCollections.observableArrayList();
+        try {
+            Connection conn = JDBC.getConnection();//Gets connection to database
+            String selectUsers = "SELECT * FROM users";//Select statement
+
+            DBQuery.setPreparedStatement(conn, selectUsers);//sets prepared statement to be the select statement
+            PreparedStatement ps = DBQuery.getPreparedStatement();//creates prepared statement ps
+
+            ps.execute();//executes the prepared statement
+
+            ResultSet rs = ps.getResultSet();//this is the result set of the prepared statement
+
+            users user;
+            while(rs.next()){//rs.next() goes to the next item or line of the result set rs
+                user = new users(rs.getInt("User_ID"),//collects info from database based on column names from database
+                        rs.getString("User_Name"),
+                        rs.getString("Password"),
+                        rs.getDate("Create_Date").toLocalDate(),
+                        rs.getTime("Create_Date").toLocalTime(),
+                        rs.getString("Created_By"),
+                        rs.getDate("Last_Update").toLocalDate(),
+                        rs.getTime("Last_Update").toLocalTime(),
+                        rs.getString("Last_Updated_By")
+
+                );
+                usersList.add(user);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage()); //getMessage will print out the exception found
+        }
+        return usersList;
+
+    }
 
     @FXML
     void onActionCloseProgram(ActionEvent event)  {
@@ -59,10 +97,19 @@ public class loginMenuController implements Initializable {
         String userName = usernameTxt.getText();
         String password = passwordTxt.getText();
 
+        String correctUserName = null;
 
-        //so long as the username textbox has characters, it will begin the try catch
-        if(userName.isBlank() == false){
-            try{
+        //this will reassign the correctUserName to the username that corresponds to the db
+        for(users user : usersList()){
+            if(Objects.equals(userName, user.getUserName())){
+                correctUserName = userName;
+                break;
+            }
+        }
+
+        //so long as the correctUserName is reassigned, it will begin the try catch
+        if(correctUserName != null){
+            try {
                 Connection conn = JDBC.getConnection();//Gets connection to database
                 String selectCustomers = "SELECT Password FROM users WHERE User_Name = ?";//Select statement
 
@@ -70,7 +117,7 @@ public class loginMenuController implements Initializable {
                 PreparedStatement ps = DBQuery.getPreparedStatement();//creates prepared statement ps
 
                 //this will complete the selectStatement with the username from the text box input for the WHERE clause
-                ps.setString(1,userName);
+                ps.setString(1, userName);
 
                 ps.execute();//executes the prepared statement
 
@@ -80,8 +127,10 @@ public class loginMenuController implements Initializable {
                 while (rs.next()) {
                     String passwordDB = rs.getString("Password");
 
-                    //so long as the input password matches the database password and the username text box is not empty, this will navigate to the appointmentCalendar
-                    if (password.equals(passwordDB) && userName.isBlank() == false) {
+
+
+                    //so long as the input password matches the database password this will navigate to the appointmentCalendar
+                    if (password.equals(passwordDB)) {
                         stage = (Stage) ((Button) event.getSource()).getScene().getWindow();//cast the window as a stage
                         scene = FXMLLoader.load(getClass().getResource("/view/appointmentCalendar.fxml"));//stage(location) the button should go to
                         stage.setScene(new Scene(scene));
@@ -89,17 +138,16 @@ public class loginMenuController implements Initializable {
 
                         break;
                     }
-                    //this is when the password input and password in the database do not match, and the username text box is not empty
+                    //this is when the password input and password in the database do not match
                     //AN ERROR MESSAGE WILL BE DISPLAYED
-                    else if(password != passwordDB && userName.isBlank() == false){
-                        System.out.println("Incorrect password");
+                    else if (password != passwordDB) {
+                        //System.out.println("Incorrect password");
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setTitle("Error Dialog");
+                        alert.setContentText("Incorrect password. Please try again.");
+                        alert.showAndWait();
 
                         break;
-                    }
-                    //when the username text box is empty once the loop has begun
-                    //AN ERROR MESSAGE WILL BE DISPLAYED
-                    else {
-                        System.out.println("Incorrect username");
                     }
 
                 }
@@ -110,8 +158,14 @@ public class loginMenuController implements Initializable {
             }
 
         }
+        //when the correctUserName is not reassigned because the input in the userNameTxt does not match the usernames found in the db
+        //AN ERROR MESSAGE WILL BE DISPLAYED
         else{
-            System.out.println("Incorrect username");
+            //System.out.println("Incorrect username");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setContentText("Incorrect username. Please try again.");
+            alert.showAndWait();
         }
 
 
